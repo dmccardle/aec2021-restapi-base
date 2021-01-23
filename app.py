7268a13e5c5d222e8f3576aab07e895fabf5c001
Evan Fitzgerald <evandfitz@gmail.com>
2021-01-23 10:58:23 -0400

    parsed car data

--- app.py
+++ app.py
@@ -1,14 +1,55 @@
 from flask import Flask, jsonify, request 
 from flask_cors import CORS
+from flask_marshmallow import Marshmallow
 import os
 app = Flask(__name__) 
 CORS(app)
 
+ma = Marshmallow(app)
+
+# classes
+class Car():
+    def __init__(self, name, carType, carRange, cost, seats, link):
+        self.name = name
+        self.carType = carType
+        self.carRange = carRange
+        self.cost = cost
+        self.seats = seats
+        self.link = link
+
+class CarSchema(ma.Schema):
+    class Meta:
+        fields = ('name', 'carType', 'carRange', 'cost', 'seats', 'link')
+
+list_of_cars = []
+car_schema = CarSchema(many=True)
+
+# does parsing
+def parse_car_data():
+    data_file = open("vehicles.txt", "r")
+    Lines = data_file.readlines()
+    for i in range(2, 11):
+        car = parse_car_from_data_lines(Lines[i])
+        list_of_cars.append(car)
+    return car_schema.jsonify(list_of_cars)
+
+# builds and returns a City object based on the list of data provided
+def parse_car_from_data_lines(car_data_lines):
+    carList = car_data_lines.split(",")
+    name = carList[0]
+    carType = carList[1]
+    carRange = carList[2]
+    cost = carList[3]
+    seats = carList[4]
+    link = carList[5]
+    car = Car(name, carType, carRange, cost, seats, link)
+    return car
 
 @app.route('/', methods = ['GET', 'POST']) 
 def home():
-  data_file = open("SJ.txt", "r")
-  return jsonify(data_file.readlines()[4::])
+  data_file = open("vehicles.txt", "r")
+  return parse_car_data()
+  #return jsonify(data_file.readlines()[2::])
 
 # driver function 
 if __name__ == '__main__': 
