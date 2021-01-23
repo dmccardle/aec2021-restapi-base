1e5ca74379a28d276e6009138b5e12a433fd2099
Evan Fitzgerald <evandfitz@gmail.com>
2021-01-23 15:03:27 -0400

    update tree

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ ma = Marshmallow(app)
 
 # classes
 class Car():
-    def __init__(self, name, carType, carRange, cost, seats, saving, link):
+    def __init__(self, name, carType, carRange, cost, seats, saving, batterySize, link, costAnalysis, emissions):
         self.name = name
         self.carType = carType
         self.carRange = carRange
@@ -17,12 +17,35 @@ class Car():
         self.seats = seats
         self.saving = saving
         self.link = link
+        self.batterySize = batterySize
+        self.costAnalysis = costAnalysis
+        self.emissions = emissions
 
 class CarSchema(ma.Schema):
     class Meta:
-        fields = ('name', 'carType', 'carRange', 'cost', 'seats', 'saving', 'link')
+        fields = ('name', 'carType', 'carRange', 'cost', 'seats', 'saving', 'batterySize', 'link', 'costAnalysis', 'emissions')
 
 list_of_cars = []
+
+# $/kWH
+prov =	{
+    "AB" : 16.7,
+    "BC" : 12.4,
+    "MB" : 9.6,
+    "NB" : 12.7,
+    "NL" : 13.8,
+    "NS" : 15.0,
+    "NT" : 38.7,
+    "NU" : 37.5,
+    "ON" : 12.5,
+    "PE" : 16.8,
+    "QC" : 7.3,
+    "SK" : 18.2,
+    "YT" : 14.5
+}
+
+
+gasCharge = 1 # $/litre
 car_schema = CarSchema(many=True)
 
 # does parsing
@@ -34,7 +57,7 @@ def parse_car_data():
         list_of_cars.append(car)
     return car_schema.jsonify(list_of_cars)
 
-# builds and returns a City object based on the list of data provided
+# builds and returns a Car object based on the list of data provided
 def parse_car_from_data_lines(car_data_lines):
     carList = car_data_lines.split(",")
     name = carList[0]
@@ -43,9 +66,46 @@ def parse_car_from_data_lines(car_data_lines):
     cost = carList[3]
     seats = carList[4]
     saving = carList[5]
-    link = carList[6]
-    car = Car(name, carType, carRange, cost, seats, saving, link)
+    batterySize = carList[6]
+    emissions = 0
+    link = carList[7]
+    costAnalysis = []
+    car = Car(name, carType, carRange, cost, seats, saving, batterySize, link, costAnalysis, emissions)
     return car
+
+# calculates cost analysis for gas and electrical cars
+def cost_analysis(kilo_per_year, location):
+    electricalCharge = float(prov[location]/100)
+    for x in range(0, 5):
+        for i in list_of_cars:
+            if i.carType == 'ZEV':
+                charges = kilo_per_year / int(i.carRange)
+                chargeCost = electricalCharge * float(i.batterySize)
+                cost = charges * chargeCost
+            else:
+                litres = (kilo_per_year / 100) * float(i.carRange)
+                cost = gasCharge * litres
+            i.costAnalysis.append(cost)
+
+# calculates emissions
+def emission_analysis():
+    for i in list_of_cars:
+        if i.carType != 'ZEV':
+            i.emissions = (2.33 * float(i.carRange) / 100) + (0.43 * float(i.carRange) / 100) # kg of co2 per km
+        
+def failCar(x):
+    if len(x) > 1:
+        minCar = x[0]
+        min = 999999
+        for i in x:
+            if sum(i.costAnalysis) < int(min):
+                min = sum(i.costAnalysis)
+                minCar = i
+    
+        for i in x:
+            if sum(i.costAnalysis) != min:
+                x.remove(i)
+
 
 @app.route('/', methods = ['GET', 'POST']) 
 def home():
@@ -54,10 +114,12 @@ def home():
 
 @app.route('/output', methods = ['GET', 'POST'])
 def output():
+
   # 1. receive input from user (will fake for now)    
-    cost = 40000 # consider range
-    location = 'New Brunswick' # province
-    kperYear = 40000 #(high, med, low) -> high 
+    minCost = 10000 # consider range
+    maxCost = 35000 # consider range
+    location = 'NB' # province
+    kilo_per_year = 40000 #(high, med, low) -> high 
     numSeats = 5 # consider range
 
     trueList = list_of_cars
@@ -66,10 +128,90 @@ def output():
     for i in trueList:
         i.cost = int(i.cost) - int(i.saving)
 
-    if cost < 43000:
-        for i in trueList:
-            if i.cost >= 43000:
-                trueList.remove(i)
+    # cost analysis
+    cost_analysis(kilo_per_year, location)
+    emission_analysis()
+    #return car_schema.jsonify(list_of_cars)
+
+    # split lists into electrical and gas
+    gasList = []
+    elecList = []
+
+    for i in trueList:
+        if i.carType == 'ZEV':
+            elecList.append(i)
+        else:
+            gasList.append(i)
+
+    # gas List
+    count = 0
+    for i in gasList:
+        if int(i.cost) <= maxCost and int(i.cost) >= minCost:
+            count = count + 1
+
+    # check if any in budget
+    if (count != 0):
+        for i in gasList:
+            if int(i.cost) > maxCost or int(i.cost) < minCost:
+                gasList.remove(i)
+        
+        count = 0
+        for i in gasList:
+            if int(i.seats) == numSeats:
+                count = count + 1
+
+
+
+        # check if seat num is good
+        if (count != 0):
+            for i in gasList:
+                if int(i.seats) != numSeats:
+                    gasList.remove(i)
+            
+            failCar(gasList)
+        else:
+            failCar(gasList)
+    else:
+        failCar(gasList)
+                
+    minCost = 10000 
+    maxCost = 35000 
+
+    # elec List
+    count = 0
+    for i in elecList:
+        if int(i.cost) <= maxCost and int(i.cost) >= minCost:
+            count = count + 1
+
+    # check if any in budget
+    if count != 0:
+        for i in elecList:
+            if int(i.cost) > maxCost or int(i.cost) < minCost:
+                elecList.remove(i)
+
+        return car_schema.jsonify(elecList)     
+        
+        count = 0
+        for i in elecList:
+            if int(i.seats) == numSeats:
+                count = count + 1
+        
+        # check if seat num is good
+        if (count != 0):
+            for i in elecList:
+                if int(i.seats) != numSeats:
+                    elecList.remove(i)
+
+            return car_schema.jsonify(elecList)
+
+            failCar(elecList)
+        else:
+            failCar(elecList)
+    else:
+        failCar(elecList)
+
+    retList = [elecList[0], gasList[0]]
+    return car_schema.jsonify(retList)
 
     #return 5
 
@@ -77,3 +219,13 @@ def output():
 # driver function 
 if __name__ == '__main__': 
 	app.run(debug = True) 
+
+
+#Notes:
+
+#receive JSON object -> send back JSON object
+
+    #POST message to be recieved#
+    # in teams
+
+    # return one gas and one electric
